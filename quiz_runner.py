e95ccb78b3ed097f1c2ddde21ecb4adb66fdd94f
Angela <angeladejuan55@gmail.com>
2025-05-09 16:41:18 +0800

    Extracted lines for category, question, choices, and answer

--- quiz_runner.py
+++ quiz_runner.py
@@ -40,6 +40,13 @@ def load_questions_from_text(file_path):
          if raw_lines[line_index] == "":
             line_index += 1
             continue
+         
+          try:
+            category_line = raw_lines[line_index]
+            question_line = raw_lines[line_index + 1]
+            choice_lines = raw_lines[line_index + 2:line_index + 6]
+            answer_line = raw_lines[line_index + 6]
+            
         #Add the question to the question list
 #Create a function that will display the questions, as well as the choices
 #Create a function that will ask user to input their answer to the question
