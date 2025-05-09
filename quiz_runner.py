7c5e7ce5c42a44d452ae47d465a2c3d23f72ebef
Angela <angeladejuan55@gmail.com>
2025-05-09 16:15:06 +0800

    Pseudocode created

---
+++ quiz_runner.py
@@ -0,0 +1,23 @@
+#Importing libraries
+#Adding welcome message with typing effect
+#Asking the user to choose the question file
+#Load questions from the text file
+    #Open the files in read mode
+    #Read all lines from the file
+    #Initialize an empty list to store the questions
+    #Loop thru the lines in the file
+        #Add the question to the question list
+#Create a function that will display the questions, as well as the choices
+#Create a function that will ask user to input their answer to the question
+#Create a function that will start the quiz
+    #Initialize score as zero
+    #Shuffle the list of all loaded questions
+    #If user entered Q, print quit message and break the loop
+    #Otherwise, check if the user's answer is correct 
+#Define function: main()
+    #Show welcome message
+    #Call choose_file() to get file path
+    #If no file chosen, show error and exit
+    #Call load_questions_from_text()
+    #If questions loaded, call start_quiz()
+#Call main() to begin execution
